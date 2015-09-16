0c3646566e4b0645f544129a311df35d04a788f1
lncwwn <lncwwn@gmail.com>
2015-09-16 23:43:04 +0800

    added react support

--- public/js/dazhe_entry.js
+++ public/js/dazhe_entry.js
@@ -1,9 +1,22 @@
 var React = require('react');
+var Router = require('react-router');
+var Routes = Router.Routes;
+var Route = Router.Route;
+
 var DaZheApp = React.createClass({
     render: function() {
         return (
-            <div></div>
+            <div>
+                <h3>hello world</h3>
+            </div>
         );
     }
 });
 
+var routes = (
+    <Route handler={DaZheApp}></Route>
+);
+
+Router.run(routes, Router.HashLocation, function(Root) {
+    React.render(<Root />, document.getElementById('app'));
+});
